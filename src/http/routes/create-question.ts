944247fcae3cbaec45d034fff806b636db2b397d
maarcos4g <marcospaullo552@gmail.com>
2025-07-09 11:43:10 -0300

    feat: audio upload & reply

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -2,6 +2,8 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
 import { z } from "zod/v4";
+import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts";
+import { and, eq, sql } from "drizzle-orm";
 
 export const createQuestion: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -20,11 +22,37 @@ export const createQuestion: FastifyPluginCallbackZod = (app) => {
       const { question } = request.body
       const { roomId } = request.params
 
+      const embeddings = await generateEmbeddings(question)
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          simalarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+        })
+        .from(schema.audioChunks)
+        .where(and(
+          eq(schema.audioChunks.roomId, roomId),
+          sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+        ))
+        .orderBy(sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`)
+        .limit(3)
+
+      let answer: string | null = null
+      
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map(chunk => chunk.transcription)
+        answer = await generateAnswer(question, transcriptions)
+      }
+
       const result = await db
         .insert(schema.questions)
         .values({
           question,
-          roomId
+          roomId,
+          answer
         }).returning()
 
       const insertedQuestion = result[0]
@@ -34,7 +62,8 @@ export const createQuestion: FastifyPluginCallbackZod = (app) => {
       }
 
       return reply.status(201).send({
-        questionId: insertedQuestion.id
+        questionId: insertedQuestion.id,
+        answer,
       })
     })
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
 import {
   serializerCompiler,
   validatorCompiler,
@@ -10,12 +11,15 @@ import { getRooms } from "./routes/get-rooms.ts";
 import { createRoom } from "./routes/create-room.ts";
 import { getRoomQuestions } from "./routes/get-room-questions.ts";
 import { createQuestion } from "./routes/create-question.ts";
+import { uploadAudio } from "./routes/upload-audio.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: '*'
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -29,6 +33,7 @@ app.register(getRooms)
 app.register(createRoom)
 app.register(getRoomQuestions)
 app.register(createQuestion)
+app.register(uploadAudio)
 
 app.listen({
   port: env.PORT,
